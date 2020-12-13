cabc4f3d75beb4dbf2a406fcdfeb5397c0c888c7
Lưu Quý Nhân <nhanlq.bi9178@st.usth.edu.vn>
2020-12-14 01:04:35 +0700

    Finished all practical works
    Also fixed some conflicts between PC ver and Laptop ver

---
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/MainActivity.java
@@ -0,0 +1,48 @@
+package vn.edu.usth.weather;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("Weether", "onCreate");
+        ForecastFragment firstFragment = new ForecastFragment();
+        getSupportFragmentManager().beginTransaction().add(R.id.container, firstFragment).commit();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("Weether", "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("Weether", "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("Weether", "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("Weether","onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("Weether", "onDestroy");
+    }
+}
