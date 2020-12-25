4985cbf382068f691491128357a10f2d88709313
Lưu Quý Nhân <nhanlq.bi9178@st.usth.edu.vn>
2020-12-25 23:50:30 +0700

    Practical work 11

--- USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -2,17 +2,48 @@ package vn.edu.usth.weather;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
+import android.widget.Toast;
+
 import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
 import com.google.android.material.tabs.TabLayout;
 
-import vn.edu.usth.weather.HomeFragmentPagerAdapter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class WeatherActivity extends AppCompatActivity {
 
+    MediaPlayer mediaPlayer;
+
+    private void copyFileToExternalStorage(int resourceId, String resourceName) {
+        try {
+            File file = new File(getExternalFilesDir(null), resourceName);
+            InputStream in = getApplicationContext().getResources().openRawResource(resourceId);
+            OutputStream out = new FileOutputStream(file);
+            byte[] buff = new byte[1024 * 2];
+            int read = 0;
+            try {
+                while ((read = in.read(buff)) > 0) {
+                    out.write(buff, 0, read);
+                }
+            }
+            finally {
+                in.close();
+                out.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,29 +57,38 @@ public class WeatherActivity extends AppCompatActivity {
 
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tab);
         tabLayout.setupWithViewPager(pager);
+
+
+        mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.music);
+
     }
 
     @Override
     protected void onStart() {
         super.onStart();
+        mediaPlayer.start();
+        copyFileToExternalStorage(R.raw.music, "music.mp3");
         Log.i("Weether", "onStart");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+        mediaPlayer.start();
         Log.i("Weether", "onResume");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
+        mediaPlayer.pause();
         Log.i("Weether", "onPause");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
+        mediaPlayer.stop();
         Log.i("Weether","onStop");
     }
 
